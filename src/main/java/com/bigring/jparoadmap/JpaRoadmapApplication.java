1c21ba21523fe2e482a1d692bf92ae5fe90684e5
권대환 <bigring@wemakeprice.com>
2021-03-01 14:24:04 +0900

    create project

---
+++ src/main/java/com/bigring/jparoadmap/JpaRoadmapApplication.java
@@ -0,0 +1,13 @@
+package com.bigring.jparoadmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRoadmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRoadmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigring/jparoadmap/JpaRoadmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigring.jparoadmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRoadmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
