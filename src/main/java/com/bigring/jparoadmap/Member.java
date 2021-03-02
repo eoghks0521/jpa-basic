f46b9ece41651b7288200cd64d0fd799c0d8f55f
eoghks0521 <eoghks0521@naver.com>
2021-03-02 23:10:32 +0900

    entity mapping

--- src/main/java/com/bigring/jparoadmap/Member.java
+++ src/main/java/com/bigring/jparoadmap/Member.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @Getter
 @Setter
 @Entity
