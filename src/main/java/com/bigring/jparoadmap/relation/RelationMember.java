3a6ac3d938514f970ebc2abe5e5ee453d3e1801a
eoghks0521 <eoghks0521@naver.com>
2021-03-03 18:34:34 +0900

    unidirectional relationship

--- src/main/java/com/bigring/jparoadmap/relation/RelationMember.java
+++ src/main/java/com/bigring/jparoadmap/relation/RelationMember.java
@@ -1,2 +1,29 @@
-package com.bigring.jparoadmap.relation;public class RelationMember {
+package com.bigring.jparoadmap.relation;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class RelationMember {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 }
